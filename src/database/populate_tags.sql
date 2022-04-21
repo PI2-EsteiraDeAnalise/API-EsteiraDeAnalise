efc563f45b959ea97ffea9385215a49a186fe065
thiagogmf <thiagogmf@protonmail.com>
2022-04-21 17:17:56 -0300

    adicionando endpoint de tags

---
+++ src/database/populate_tags.sql
@@ -0,0 +1,6 @@
+INSERT INTO tags (nome) VALUES
+('resistor'),
+('capacitor'),
+('inductor'),
+('diode'),
+('transistor'),
--- src/models.py
+++ src/models.py
@@ -1,8 +1,6 @@
 from sqlalchemy import Column, Integer, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
-from datetime import datetime
-
 from extensions import db
 
 class Placa(db.Model):
@@ -10,7 +8,7 @@ class Placa(db.Model):
   __tablename__ = 'placas'
 
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  data = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+  data = db.Column(db.DateTime(), nullable=False, default=db.func.current_timestamp())
   sucesso = db.Column(db.Boolean, nullable=False)
 
   def __init__(self, sucesso):
@@ -19,9 +17,26 @@ class Placa(db.Model):
   def serialize(self):
     return {
       'id': self.id,
-      'data': self.data,
+      'data': self.data.__str__(),
       'sucesso': self.sucesso,
     }
 
   def __repr__(self):
     return '<placa: {}>'.format(self.id)
+
+class Tag(db.Model):
+
+  __tablename__ = 'tags'
+
+  nome = db.Column(db.String(50), primary_key=True, nullable=False)
+
+  def __init__(self, nome):
+    self.nome = nome
+  
+  def serialize(self):
+    return {
+      'nome': self.nome,
+    }
+  
+  def __repr__(self):
+    return '<tag: {}>'.format(self.nome)
