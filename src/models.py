bf6fa11312f34fb0b49c34a2336d40fa4604bfdc
thiagogmf <thiagogmf@protonmail.com>
2022-04-23 16:31:23 -0300

    corrigindo requisicao para fevereiro e retornando requisicao por ano

--- src/models.py
+++ src/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, DateTime, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, DateTime, Boolean
 
 from extensions import db
 
--- src/resources/boards.py
+++ src/resources/boards.py
@@ -6,6 +6,8 @@ from sqlalchemy.sql import extract
 from extensions import db
 from models import Board
 
+from utils.days_by_month import days_by_month
+
 from functools import reduce
 
 class Boards(Resource):
@@ -20,18 +22,35 @@ class Boards(Resource):
       return 0
     return reduce(lambda x, y: x + y, [1 if not board.sucess else 0 for board in boards])
   
+  def get_all_boards_in_month(self, year, month, initial_day, final_day, response):
+    initial_date = '{}-{}-{}'.format(year, month, initial_day)
+    final_date = '{}-{}-{}'.format(year, month, final_day)
+    records = Board.query.filter(Board.date >= initial_date, Board.date <= final_date).all()
+    response.append({
+        'day': '{}-{}'.format(initial_day, final_day),
+        'month': int(month),
+        'year': int(year),
+        'successfully': self.get_successfully_boards(records),
+        'unsuccessfully': self.get_unsuccessfully_boards(records),
+      })
+    return response
+  
   def request_by_year_and_month(self, year, month):
     if year is None or month is None:
       return []
     response = []
-    for k in range(1,27,5):
-      initial_date = '{}-{}-{}'.format(year, month, k)
-      final_date = '{}-{}-{}'.format(year, month, k + 4)
-      records = Board.query.filter(Board.date >= initial_date, Board.date <= final_date).all()
+    for k in range(1, 25,5):
+      self.get_all_boards_in_month(year, month, k, k+4, response)
+    self.get_all_boards_in_month(year, month, 25, days_by_month[int(month) - 1], response)
+    return response
+  
+  def request_by_year(self, year):
+    response = []
+    for k in range(1, 13):
+      records = Board.query.filter(extract('year', Board.date) == year).filter(extract('month', Board.date) == k).all()
       response.append({
-        'day': '{}-{}'.format(k, k+4),
-        'month': int(month),
-        'year': int(year),
+        'month': k,
+        'quantity': len(records),
         'successfully': self.get_successfully_boards(records),
         'unsuccessfully': self.get_unsuccessfully_boards(records),
       })
@@ -40,7 +59,10 @@ class Boards(Resource):
   def get(self):
     year = request.args.get('year')
     month = request.args.get('month')
-    response = self.request_by_year_and_month(year, month)
+    if not (month is None) and not (year is None):
+      response = self.request_by_year_and_month(year, month)
+    elif not (year is None):
+      response = self.request_by_year(year)
     if len(response) == 0:
       return make_response(jsonify({'message': 'Nenhum registro encontrado'}), 404)
     return make_response(jsonify(response), 200)
