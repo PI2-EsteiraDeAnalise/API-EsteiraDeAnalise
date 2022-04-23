526b9b1e0d7b785cb0b2e9bdbcb8e5a7b77a964e
thiagogmf <thiago.guilherme20@hotmail.com>
2022-04-23 15:28:04 -0300

    corrigindo model de metricas

--- src/models.py
+++ src/models.py
@@ -46,7 +46,7 @@ class Metric(db.Model):
   __tablename__ = 'metrics'
 
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  metric = db.Column(db.Integer, nullable=False)
+  metric = db.Column(db.String(50), nullable=False)
   description = db.Column(db.String(200), nullable=False)
 
   def __init__(self, metric, description):
