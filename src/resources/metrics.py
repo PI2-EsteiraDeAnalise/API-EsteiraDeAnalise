f63077bedc1a29a66b1c8063a221bc8765f35a2e
thiagogmf <thiago.guilherme20@hotmail.com>
2022-04-24 21:39:42 -0300

    passando dados para requisicao get para um txt

--- src/resources/metrics.py
+++ src/resources/metrics.py
@@ -2,23 +2,26 @@ from flask_restful import Resource
 from flask import jsonify, request, make_response
 
 from extensions import db
-from models.metric import Metric
-
 
 class Metrics(Resource):
-    def get(self):
-        records = Metric.query.all()
-        if len(records) == 0:
-            return make_response(
-                jsonify({"message": "Nenhum registro encontrado"}), 404
-            )
-        return make_response(
-            jsonify([Metric.serialize(record) for record in records]), 200
-        )
 
-    def post(self):
-        args = request.get_json()
-        metric_record = Metric(metric=args["metric"], description=args["description"])
-        db.session.add(metric_record)
-        db.session.commit()
-        return Metric.serialize(metric_record), 201
+  def read_file(self):
+    with open('/app/src/assets/infos.txt', 'r') as f:
+      return f.readlines()
+ 
+  def serialize(self, records):
+    response = []
+    for record in records:
+      response.append(
+        {
+          'metric': record.split(',')[0],
+          'description': record.split(',')[1].replace('\n', ''),
+        }
+      )
+    return jsonify(response)
+  
+  def get(self): 
+    records = self.read_file()
+    return make_response(self.serialize(records), 200)
+  
+ 
